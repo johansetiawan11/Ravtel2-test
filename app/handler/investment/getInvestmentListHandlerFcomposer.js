b1305fbde5452bf41a88d51c8ff1f6a06f3a12b0
johansetiawan11 <johan@maven.co.idd>
2021-11-15 13:42:09 +0700

    integrate ORM in investment
    Rebase fix

--- app/handler/investment/getInvestmentListHandlerFcomposer.js
+++ app/handler/investment/getInvestmentListHandlerFcomposer.js
@@ -1,29 +1,50 @@
+const dbTableName = require("../../const/dbTableName");
+const errorSid = require("../../const/errorSid");
+
 function getInvestmentListHandlerFcomposer(diHash) {
+  const {
+    dbModelHash,
+    sqlCommon,
+  } = diHash;
+  const {
+    Investment,
+  } = dbModelHash;
+
   async function getInvestmentListHandler(req, res) {
-    const { dataMock } = diHash;
-    const { investmentList } = dataMock;
-    const query = req.query;
     try {
-      if (Object.keys(query).length > 0) {
-        const data = investmentList.filter(
-          (obj) => (obj.accountId === query["account-id"]),
-        );
-        res.status(200).json({
-          count: data.length,
-          investment: data,
-        });
-      } else {
-        res.status(200).json({
-          count: investmentList.length,
-          investment: investmentList,
-        });
-      }
-    } catch (error) {
-      res.status(500).send({
-        message: error.message,
+      const query = req.query;
+      const pageNumber = sqlCommon.sanitizePageNumber(query.page);
+      const pageLength = sqlCommon.sanitizePageLength(query.length);
+      const sortDirection = sqlCommon.sanitizeSortDirection(query.direction);
+      const orderByRawSql = `${dbTableName.INVESTMENT}.id ${sortDirection} NULLS LAST`;
+
+      const investmentQueryResponse = await Investment
+      .query()
+      .whereNull("deleted_at")
+      .orderByRaw(orderByRawSql)
+      .page(pageNumber - 1, pageLength)
+      .where((q) => {
+        if (query["s-account-id"]) {
+          q.andWhere("account_id", query["s-account-id"]);
+        }
+      });
+
+      return res.status(200).json({
+        data: {
+          records: investmentQueryResponse.results,
+          total: investmentQueryResponse.total,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+
+      return res.status(503).send({
+        message: errorSid.GET_INVESTMENT_LIST_HANDLER_OPERATION_FAILED,
+        debugInfo: err.message,
       });
     }
   }
+
   return getInvestmentListHandler;
 }
 
--- app/handler/investment/putInvestmentHandlerFcomposer.js
+++ app/handler/investment/putInvestmentHandlerFcomposer.js
@@ -1,27 +1,91 @@
-function putInvestmentHandlerFcomposer(diHash) {
-  const { dataMock } = diHash;
-  const { investmentList } = dataMock;
-  async function putInvestmentHandler(req, res) {
-    const investment = investmentList.find((item) => (item.id).toString() === req.params.id);
-    try {
-      investment.accountId = req.body.accountId;
-      investment.productId = req.body.productId;
-      investment.lastUnitAmount = req.body.lastUnitAmount;
-      investment.hwm = req.body.hwm;
-      investment.performanceFeePercent = req.body.performanceFeePercent;
-      investment.penaltyFeePercent = req.body.penaltyFeePercent;
-      investment.invesmentDate = req.body.invesmentDate;
-      investment.nextPerformanceFeeDate = req.body.nextPerformanceFeeDate;
-      investment.maturityDate = req.body.maturityDate;
-      investment.updated_at = new Date(Date.now()).toLocaleString().split(",")[0];
+const errorSid = require("../../const/errorSid");
 
-      res.status(200).send({
-        investment,
+function putInvestmentHandlerFcomposer(diHash) {
+  const {
+    dbModelHash,
+    objection,
+  } = diHash;
+  const {
+    Investment,
+  } = dbModelHash;
+  const {
+    DataError,
+    UniqueViolationError,
+  } = objection;
+
+  async function putInvestmentHandler(req, res) {
+    try {
+      const params = req.params;
+      const investmentId = params.id;
+      const body = req.body;
+
+      const investmentUuidQueryResponse = await Investment
+      .query()
+      .where("id", investmentId)
+      .whereNull("deleted_at");
+      if (!(investmentUuidQueryResponse && (investmentUuidQueryResponse.length > 0))) {
+        return res.status(404).send({
+          message: errorSid.PUT_investment_HANDLER_investment_RECORD_NOT_FOUND,
+        });
+      }
+      const existinginvestmentRecord = investmentUuidQueryResponse[0];
+      const existinginvestmentRecordId = existinginvestmentRecord.id;
+
+      const investmentUpdateArgHash = {
+        account_id: body.accountId,
+        product_id: body.productId,
+        last_unit_amount: req.body.lastUnitAmount,
+        hwm: body.hwm,
+        performance_fee_percent: body.performanceFeePercent,
+        penalty_fee_percent: body.penaltyFeePercent,
+        investment_date: body.invesmentDate,
+        next_performance_fee_date: body.nextPerformanceFeeDate,
+        maturity_date: body.maturityDate,
+        updated_at: new Date().toISOString(),
+        updated_by: "system",
+      };
+      const investmentUpdateResponse = await Investment
+      .query()
+      .patchAndFetchById(existinginvestmentRecordId, investmentUpdateArgHash);
+
+      return res.status(200).json({
+        data: investmentUpdateResponse,
       });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        message: error.message,
+    } catch (err) {
+      console.error(err);
+
+      let debugInfo;
+
+      if (err instanceof DataError) {
+        if (
+          err.nativeError
+          && err.nativeError.routine
+        ) {
+          debugInfo = {
+            routine: err.nativeError.routine,
+          };
+        }
+
+        return res.status(400).send({
+          message: errorSid.PUT_INVESTMENT_HANDLER_ID_INVALID,
+          debugInfo,
+        });
+      } else if (err instanceof UniqueViolationError) {
+        if (err.constraint) {
+          debugInfo = {
+            constraint: err.constraint,
+          };
+        }
+
+        return res.status(409).send({
+          message: errorSid.PUT_INVESTMENT_HANDLER_OPERATION_CONFLICTED,
+          debugInfo,
+        });
+      }
+
+      return res.status(503).send({
+        message: errorSid.PUT_INVESTMENT_HANDLER_OPERATION_FAILED,
+        debugInfo: err.message,
       });
     }
   }
